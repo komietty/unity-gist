353cbe6db0a0f0ab5374a2795ffa059942966b37
komietty <komietty@gmail.com>
2019-08-25 22:20:17 +0900

    add recorder

---
+++ Cginc/Recorder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace kmty.gist {
+    public class Recorder : MonoBehaviour {
+
+        public int framerate = 60;
+        public int maxRecordSeconds = 180;
+        public bool recode = false;
+        int frameCount;
+
+        void Start() {
+            if (recode) StartRecording();
+        }
+
+        void StartRecording() {
+            System.IO.Directory.CreateDirectory("Capture");
+            Time.captureFramerate = framerate;
+            frameCount = -1;
+        }
+
+        void Update() {
+            if (frameCount > 0 && frameCount < framerate * maxRecordSeconds && recode)
+                ScreenCapture.CaptureScreenshot($"Capture/frame{frameCount.ToString("0000")}.png");
+            frameCount++;
+        }
+    }
+}
