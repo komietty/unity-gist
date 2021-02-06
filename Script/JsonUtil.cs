7a1c18ff9ac019b58ffa0b8fbbccb0deb9ec3efc
sakikomikado <komietty@gmail.com>
2021-02-06 19:40:19 +0900

    update jsonutil

--- Script/JsonUtil.cs
+++ Script/JsonUtil.cs
@@ -3,8 +3,9 @@ using System.IO;
 
 namespace kmty.gist {
     public static class JsonUtil {
-        public static string LoadData(string filename) {
-            var path = Path.Combine(Application.streamingAssetsPath, filename);
+        public static string LoadData(string filename, string dir = default) {
+            if (dir == default) dir = Application.streamingAssetsPath;
+            var path = Path.Combine(dir, filename);
             var text = "";
             if (File.Exists(path)) text = File.ReadAllText(path);
             else Debug.LogWarning(path + " is not found");
